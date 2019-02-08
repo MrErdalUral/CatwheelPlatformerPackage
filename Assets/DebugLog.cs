52e3a9393863b8ebc3c8572e099811e5ddef7201
MrErdalUral <mrerdalural@gmail.com>
2019-02-08 12:27:45 +0300

    made level transitions trigger on collision with level borders on level end state
    https://gfycat.com/AlertGeneralJackal

---
+++ Assets/DebugLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+    public string DebugMessage;
+
+    public void DebugLogMessage()
+    {
+        Debug.Log(DebugMessage);
+    }
+}
